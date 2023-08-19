013cee53d15230e418eb29b3bc3c4c60ef953402
mahmoud <mahmoud123@gmail.com>
2023-08-20 01:03:09 +0300

    upload intial project fiels

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const moonIcon = document.getElementById("moonIcon");
+const sunIcon = document.getElementById("sunIcon");
+const scrollBtn = document.getElementById("scrollBtn");
+function changeMood() {
+    moonIcon.addEventListener("click", () => {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "block";
+    });
+    sunIcon.addEventListener("click", () => {
+        moonIcon.style.display = "block";
+        sunIcon.style.display = "none";
+    });
+}
+changeMood();
+
+window.addEventListener(('scroll'), () => {
+    if (window.scrollY >= 500) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+})
+scrollBtn.addEventListener('click',()=>{
+    window.scrollTo({top:1,behavior:'smooth'}) ;
+})
